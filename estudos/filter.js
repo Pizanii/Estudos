7410098563f7c04a37301327dbf044bec977d32d
pizanii <ryanpizanisj@gmail.com>
2021-08-23 19:26:34 -0300

    atualizacao

--- estudos/filter.js
+++ estudos/filter.js
@@ -19,7 +19,7 @@
 
 const numeros = [1, 2, 3, 4, , 5, 6, 7, 8, 9, 10];
 function buscarNumerosPares(value) {
-  if (value % 2 == 0) return value;
+  if (value % 3 == 0) return value;
 }
 var numerosPares = numeros.filter(buscarNumerosPares);
 console.log(numerosPares);
